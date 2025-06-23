81db2a2ba4a83325847d26fa8cafb6c34b5eb082
SirSkye <aisha278151@gmail.com>
2025-06-23 13:16:53 -0400

    Added Vector/Vertex

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,24 @@
 from collections import namedtuple
 
-Point = namedtuple("Point", ['x', 'y'])
-Colour = namedtuple("Colour", ['r', 'g', 'b'])
-Vertex = namedtuple("Vertex", ["point", "colour"])
 BarycentricPoint = namedtuple("Bary", ["alpha", "beta", "gamma"])
+
+class Vector:
+    def __init__(self, x: int, y: int) -> None:
+          self.x = x
+          self.y = y
+    
+    def add(self, v: "Vector") -> "Vector":
+          return Vector(self.x + v.x, self.y + v.y)
+
+    def subtract(self, v: "Vector") -> "Vector":
+         return Vector(self.x - v.x, self.y, v.y)  
+    
+    def scale(self, v: float) -> "Vector":
+         return Vector(self.x * v, self.y * v)
+
+class Vertex(Vector):
+     def __init__(self, x:int, y:int, r:int, g:int, b:int) -> None:
+          super().__init__(x, y)
+          self.red = r
+          self.blue = b
+          self.green = g
